3ce0678ed5b06ff13add3de7f644f9500e227693
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2019-02-27 11:35:59 +0000

    Add files via upload

---
+++ studyBuddy/studyBuddy/studyBuddy/auViewController.swift
@@ -0,0 +1,48 @@
+//
+//  auViewController.swift
+//  studyBuddy
+//
+//  Created by Isa Muhammad on 20/02/2019.
+//  Copyright © 2019 Isa Muhammad. All rights reserved.
+//
+
+import UIKit
+
+class auViewController: UIViewController {
+
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func addItem(_ sender: Any)
+    {
+        if(input.text != "")
+        {
+            list2.append(input.text!)
+            input.text = ""
+        }
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
